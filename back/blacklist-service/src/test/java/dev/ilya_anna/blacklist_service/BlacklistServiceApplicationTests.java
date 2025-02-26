5d0fdc6ed8e76f5f89847b108407ae5187a353e0
Ilya Ivanov <ivn25ov@mail.ru>
2025-02-26 16:05:43 +0400

    initial commit

---
+++ back/blacklist-service/src/test/java/dev/ilya_anna/blacklist_service/BlacklistServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ilya_anna.blacklist_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlacklistServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
